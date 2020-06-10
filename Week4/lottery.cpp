f0790b3564fc25e69aff10ff657645055c6f433d
Ashutosh Muley <muleyashutosh@gmail.com>
2020-06-10 13:30:35 +0530

    Week4 Commit

--- Week4/lottery.cpp
+++ Week4/lottery.cpp
@@ -1,57 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-
-
-int count_winnings(vector <pair<int, int>> segments, int l, int h, int key) {
+int rsearch(vector <int> end, int l, int h, int key) {
     int count = 0;
     if(l <= h) {
-        int mid = (l + h) / 2;
- 
-        if(key >= segments[mid].first && key <= segments[mid].second) {
-            count++;
+        int mid = l + (h - l) / 2;
+        if(end[mid] < key) {
+            
+            if(l == h)
+                return 1;
+            count = (mid - l) + 1;
+            count += rsearch(end, mid + 1, h, key);
         }
-        if(l == h) {
-            return count;
+        else {
+            count += rsearch(end, l, mid - 1, key);
         }
-        
-        if(key < segments[mid].first)
-            count += count_winnings(segments, l, mid-1, key);
-        else if (key > segments[mid].second)
-            count += count_winnings(segments,mid+1 , h, key);
-        
+    }
+    return count;
+}
 
+
+int lsearch(vector <int> start, int l, int h, int key) {
+    int count = 0;
+    if(l <= h) {
+        int mid = l + (h - l) / 2;
+        if(start[mid] > key) {
+            if(l == h)
+                return 1;
+            count = (h - mid) + 1;
+            count += lsearch(start, l, mid-1, key);
+        }
+        else {
+            count += lsearch(start, mid+1, h, key);
+        }   
     }
     return count;
 }
 
 
 
-int main () {
+int main() {
     int s, p;
     cin >> s >> p;
-    vector <pair<int, int>> segments(s);
-    for(int i = 0 ; i < s; i++) {
-        int start,end;
-        cin >> start >> end;
-        segments[i] = make_pair(start,end);
+    vector <int> start(s), end(s);
+    vector <int> points(p);
+    for(int i = 0; i < s; i++) {
+        cin >> start[i] >> end[i];
     }
-    // cout << endl;
-    // for(int i = 0; i < segments.size() ; i++) {
-    //     cout << segments[i].first << " " << segments[i].second << endl;
-    // }
-    sort(segments.begin(), segments.end());
-    // cout << "After sorting->>>>" << endl;
-    // for(int i = 0; i < segments.size() ; i++) {
-    //     cout << segments[i].first << " " << segments[i].second << endl;
-    // }
+    sort(start.begin(),start.end());
+    sort(end.begin(), end.end());
 
-    while(p--) {
-        int key;
-        cin >> key;
-        cout << count_winnings(segments,0,s-1,key) << " ";
+    for(int i = 0; i < p; i++) {
+        cin >> points[i];
+        int lc = lsearch(start, 0, s-1, points[i]);
+        int rc = rsearch(end, 0, s-1, points[i]);
+        cout << s - lc - rc << " ";
     }
+    return 0;
 }
